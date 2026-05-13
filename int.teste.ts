1030d7e8586c267a4bd964508c1eca5aab510c18
guara <dev.digoguara>
2026-05-13 08:27:31 -0300

    oba

---
+++ int.teste.ts
@@ -0,0 +1,4 @@
+let data:[number,string]=[39,'guara']
+
+data.push(23,'guara')
+data.push(23)
---
+++ operadores.ts
@@ -0,0 +1,7 @@
+const a:number = 10
+const b:number = 5
+const c:number = 6
+
+console.log('A > B:' a > b)
+console.log('C < A:', c < a)
+console.log('B = C:', b === c)
