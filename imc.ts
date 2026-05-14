9264511b962f332bf6b93f6712ec53c839705fba
guara <dev.digoguara>
2026-05-14 09:48:00 -0300

    revisita_imc

--- imc.ts
+++ imc.ts
@@ -1,5 +1,23 @@
-import readline = requirer("readline-sync")
+import readline = require ("readline-sync")
 
 console.log()
 
-const numeroPeso:
+
+
+const IMC:string = readline.question('informe o seu imc:')
+const numeroPeso:number =Number(readline.question('informe o peso:'))
+const numeroAltura:number =Number(readline.question('informe a altura'))
+let situação: string="" 
+
+
+const imc = (numeroPeso / numeroAltura) * numeroAltura
+
+if (numeroAltura >= 1.90 && numeroAltura < 1.22)
+situação = "normal"
+else if (numeroPeso >= 78 && numeroAltura < 1.22)
+situação = "normal"
+else 
+situação = "desnutrido"
+
+
+console.log(`seu imc  ${imc} seu peso ${numeroPeso} sua altura  ${numeroAltura} sua situação e ${situação}`)
