d2535110647670b286ce70aa35ac68cb3afef70f
guara <dev.digoguara>
2026-05-15 11:15:20 -0300

    aprendendo_com_frutas

--- Areis.ts
+++ Areis.ts
@@ -8,6 +8,13 @@ console.log("Tamanho da array",frutas.length)
 console.log("Utima elemento", frutas[frutas.length - 1])
 
 // carrega lista
+//frutas.sort() //teicha em ordem alfabetica  
+
+frutas[2]= "melacia" //aqui você supistitui
+//frutas.push("limão")  // adiciona no fila da lista
+//frutas.unshift("melão") // adiciona no começo da lista
+frutas.pop //removi o utimo de array
+frutas.shift //removi o primeiro no array
 
 for (let i = 0; i < frutas.length; i++)
 	console.log(`${ i + 1} - ${frutas[i]}`)
---
+++ Areis_biologia.ts
@@ -0,0 +1,4 @@
+let Biologia:string[] = ["lobo cinzendo","lobo guara","coiote","cachorro","lobo das neves"]
+
+for(let i = 0; i < Biologia.length; i++)
+console.log(`${i + 1} - ${biologia[i]}`)
