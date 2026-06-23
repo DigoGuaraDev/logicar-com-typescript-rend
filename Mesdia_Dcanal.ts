2499101ed10fa015dea49ae9c7ea61c4da1474d9
digoguaradev <dev.digoguara@gemail.com>
2026-06-23 18:30:20 -0300

    B41

--- Mesdia_Dcanal.ts
+++ Mesdia_Dcanal.ts
@@ -7,4 +7,4 @@ function CauculaMaediaDeEscritos( m1:number, m2:number ):number {
 return (m1 + m2) / 2;
 }
 
-console.log("a media do canal é", CauculaMaediaDeEscritos(m1,m2) )
+console.log("a media do canal é", CauculaMaediaDeEscritos(m1,m2))
